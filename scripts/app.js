2845ac5794fad49dfd9198fa0ccbecdca8fe103a
Steven <xiangj1@outlook.com>
2019-03-28 17:33:07 -0700

    injectedForecast added

--- scripts/app.js
+++ scripts/app.js
@@ -4,10 +4,37 @@
 
   var weatherAPIUrlBase = 'https://publicdata-weather.firebaseio.com/';
 
+  var injectedForecast = {
+    key: 'newyork',
+    label: 'New York, NY',
+    currently: {
+      time: 1453489481,
+      summary: 'Clear',
+      icon: 'partly-cloudy-day',
+      temperature: 52.74,
+      apparentTemperature: 74.34,
+      precipProbability: 0.20,
+      humidity: 0.77,
+      windBearing: 125,
+      windSpeed: 1.52
+    },
+    daily: {
+      data: [
+        { icon: 'clear-day', temperatureMax: 55, temperatureMin: 34 },
+        { icon: 'rain', temperatureMax: 55, temperatureMin: 34 },
+        { icon: 'snow', temperatureMax: 55, temperatureMin: 34 },
+        { icon: 'sleet', temperatureMax: 55, temperatureMin: 34 },
+        { icon: 'fog', temperatureMax: 55, temperatureMin: 34 },
+        { icon: 'wind', temperatureMax: 55, temperatureMin: 34 },
+        { icon: 'partly-cloudy-day', temperatureMax: 55, temperatureMin: 34 }
+      ]
+    }
+  };
+
   var app = {
     isLoading: true,
     visibleCards: {},
-    selectedCities: [],
+    selectedCities: JSON.parse(localStorage.getItem('cities')) || [],
     spinner: document.querySelector('.loader'),
     cardTemplate: document.querySelector('.cardTemplate'),
     container: document.querySelector('.main'),
@@ -41,6 +68,7 @@
     var label = selected.textContent;
     app.getForecast(key, label);
     app.selectedCities.push({ key: key, label: label });
+    localStorage.setItem('cities', JSON.stringify(app.selectedCities));
     app.toggleAddDialog(false);
   });
 
@@ -149,5 +177,11 @@
       app.getForecast(key);
     });
   };
+
+  app.updateForecastCard(injectedForecast);
+  app.selectedCities.forEach((cities) => {
+    app.visibleCards[cities.key] = cities.value;
+  });
+  app.updateForecasts();
 
 })();
